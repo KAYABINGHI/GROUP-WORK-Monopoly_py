daf094c7697c3550b7b72733838701c18d11d646
Adrian <markoti2005@gmail.com>
2025-08-28 18:33:59 +0300

    G1MP-2 TASK 1 - BOARD MANAGER (created the property_price func

--- board_manager.py
+++ board_manager.py
@@ -15,4 +15,17 @@ BOARD = [
     "Atlantic Avenue", "Ventnor Avenue", "Water Works", "Marvin Gardens",
     "Go To Jail",
     "Pacific Avenue", "North Carolina Avenue", "Community Chest", "Pennsylvania Avenue", 
+      "Short Line Railroad",
+    "Chance", "Park Place", "Luxury Tax", "Boardwalk"
 ]
+
+PROPERTY_PRICES = {
+    "Mediterranean Avenue": 60, "Baltic Avenue": 60,
+    "Oriental Avenue": 100, "Vermont Avenue": 100, "Connecticut Avenue": 120,
+    "St. Charles Place": 140, "States Avenue": 140, "Virginia Avenue": 160,
+    "St. James Place": 180, "Tennessee Avenue": 180, "New York Avenue": 200,
+    "Kentucky Avenue": 220, "Indiana Avenue": 220, "Illinois Avenue": 240,
+    "Atlantic Avenue": 260, "Ventnor Avenue": 260, "Marvin Gardens": 280,
+    "Pacific Avenue": 300, "North Carolina Avenue": 300, "Pennsylvania Avenue": 320,
+    "Park Place": 350, "Boardwalk": 400
+}
